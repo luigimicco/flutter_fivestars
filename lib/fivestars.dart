6cfb95303d50da72612e2601428c98b7660b8faf
Luigi Micco <luigi.micco@gmail.com>
2022-12-18 11:10:21 +0100

    create fivestar widget

---
+++ lib/fivestars.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FiveStars extends StatefulWidget {
+  final int stars;
+  final Function onPressed;
+  const FiveStars({required this.onPressed, this.stars = 0, super.key});
+
+  @override
+  State<FiveStars> createState() => _FiveStarsState();
+}
+
+class _FiveStarsState extends State<FiveStars> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return InkWell(
+          child: getStar(index),
+          onTap: () => widget.onPressed(index),
+        );
+      }),
+    );
+  }
+
+  Icon getStar(int pos) {
+    if (pos <= widget.stars) {
+      return const Icon(
+        Icons.star,
+        color: Colors.orange,
+      );
+    } else {
+      return const Icon(
+        Icons.star_border,
+        color: Colors.grey,
+      );
+    }
+  }
+}
