0c633306e07fce2311580f59722a6d127251160f
Luigi Micco <luigi.micco@gmail.com>
2022-12-20 21:39:03 +0100

    redut widget to stateless

--- lib/ratestars.dart
+++ lib/ratestars.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RateStars extends StatefulWidget {
+class RateStars extends StatelessWidget {
   final int stars;
   final Function(int value) onPressed;
   final double? size;
@@ -13,36 +13,31 @@ class RateStars extends StatefulWidget {
       super.key});
 
   @override
-  State<RateStars> createState() => _RateStarsState();
-}
-
-class _RateStarsState extends State<RateStars> {
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
-      children: List.generate(widget.maxStars, (index) {
+      children: List.generate(maxStars, (index) {
         return InkWell(
           customBorder: const CircleBorder(),
-          child: _getStar(index),
-          onTap: () => widget.onPressed(index),
+          child: _getStar(index, size),
+          onTap: () => onPressed(index),
         );
       }),
     );
   }
 
-  Icon _getStar(int pos) {
+  Icon _getStar(int value, double? sizeIcon) {
     IconData icon = Icons.star;
     Color color = Colors.orange;
 
-    if (pos > widget.stars) {
+    if (value > stars) {
       icon = Icons.star_border;
       color = Colors.grey;
     }
     return Icon(
       icon,
       color: color,
-      size: widget.size,
+      size: sizeIcon,
     );
   }
 }
